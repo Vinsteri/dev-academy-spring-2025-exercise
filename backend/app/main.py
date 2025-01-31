8a6b0ec4cfbb7bbbf36bba2fc640ce15515c3584
Väinö Liukko <vaino.liukko@tuni.fi>
2025-02-01 00:22:38 +0200

    Add CORS middleware to allow requests from the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import electricity
 
 
 app = FastAPI(title="Electricity Data API")
+
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 # Include the router
 app.include_router(electricity.router, prefix="/api", tags=["Electricity"])
